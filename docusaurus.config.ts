a437c0c76b3cda3e51ef39628686f584b4b71566
theaiemperor <theaiemperor@gmail.com>
2025-03-10 06:50:54 +0530

    Changing configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://theaiemperor.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/bhavishyaTheVision/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
