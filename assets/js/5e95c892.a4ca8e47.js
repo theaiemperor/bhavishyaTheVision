f8cfb0f810b75a3b292899b5c1a8ad50e1620842
theaiemperor <theaiemperor@gmail.com>
2025-03-14 10:46:37 +0530

    Added New design

--- assets/js/5e95c892.a4ca8e47.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbhavishya_the_vision=self.webpackChunkbhavishya_the_vision||[]).push([[647],{5366:(e,s,a)=>{a.r(s),a.d(s,{default:()=>c});a(8101);var h=a(3526),r=a(8826),i=a(9688),n=a(8357),t=a(8290),u=a(5105);function c(e){return(0,u.jsx)(r.e3,{className:(0,h.A)(i.G.wrapper.docsPages),children:(0,u.jsx)(t.A,{children:(0,n.v)(e.route.routes)})})}}}]);
